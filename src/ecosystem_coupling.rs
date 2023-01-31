275b15e7fbaac7b4724e3d4dfbfc80a074962bab
root <root@LAPTOP-KVPIKIVR.localdomain>
2023-01-30 16:30:32 -0800

    starting ecosystem sim

---
+++ src/ecosystem_coupling.rs
@@ -0,0 +1,26 @@
+
+pub trait autotroph {
+
+}
+
+pub trait heterotroph {
+
+}
+
+pub struct red_plant {
+
+}
+impl autotroph for red_plant {}
+
+pub struct blue_plant {
+
+}
+impl autotroph for blue_plant {}
+
+pub struct red_eater {
+
+}
+
+pub struct eater_eater {
+
+}
